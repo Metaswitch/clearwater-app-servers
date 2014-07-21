d86e9fc55eea4479a95644142626ebafa83cb567
Andy Caldwell <andrew.caldwell@metaswitch.com>
2014-07-21 16:05:08 +0000

    More updates, and splitting into two files

--- include/abstract_service.h
+++ include/abstract_service.h
@@ -1,5 +1,5 @@
 /**
- * @file appserver.h  Abstract SIP Application Server interface definition
+ * @file abstractservice.h  Abstract SIP Service interface definition
  *
  * Project Clearwater - IMS in the Cloud
  * Copyright (C) 2014  Metaswitch Networks Ltd
@@ -39,8 +39,8 @@
  * as those licenses appear in the file LICENSE-OPENSSL.
  */
 
-#ifndef APPSERVER_H__
-#define APPSERVER_H__
+#ifndef ABSTRACTSERVICE_H__
+#define ABSTRACTSERVICE_H__
 
 extern "C" {
 #include <pjsip.h>
@@ -51,22 +51,21 @@ extern "C" {
 
 #include "sas.h"
 
-class AppServerTsxHelper;
-class AppServer;
-class AppServerTsx;
+class ServiceTsxHelper;
+class AbstractService;
+class AbstractServiceTsx;
 
 
-/// The AppServerTsxHelper class handles the underlying service-related
-/// processing of a single transaction for an AppServer.  Once a service has
-/// been triggered as part of handling a transaction, the related
-/// AppServerTsxHelper is inspected to determine what should be done next, e.g.
-/// forward the request, reject it, fork it etc.
+/// The ServiceTsxHelper class handles the underlying service-related processing of
+/// a single transaction.  Once a service has been triggered as part of handling
+/// a transaction, the related ServiceTsxHelper is inspected to determine what should
+/// be done next, e.g. forward the request, reject it, fork it etc.
 /// 
 /// This is an abstract base class to allow for alternative implementations -
 /// in particular, production and test.  It is implemented by the underlying
 /// service infrastructure, not by the services themselves.
 ///
-class AppServerTsxHelper
+class ServiceTsxHelper
 {
 public:
   /// Virtual destructor.
@@ -97,33 +96,60 @@ public:
   virtual pjsip_msg* clone_request(pjsip_msg* req) = 0;
 
   /// Indicate that the request should be forwarded following standard routing
-  /// rules.  Note that, even if other Route headers are added by this AS, the
-  /// request will be routed back to the S-CSCF that sent the request in the
-  /// first place after all those routes have been visited.
-  ///
+  /// rules.
+  /// 
   /// This function may be called repeatedly to create downstream forks of an
   /// original upstream request and may also be called during response processing
   /// or an original request to create a late fork.  When processing an in-dialog
   /// request this function may only be called once.
-  /// 
-  /// This function may be called while processing initial requests,
-  /// in-dialog requests and cancels but not during response handling.
   ///
-  /// @returns             - The ID of this forwarded request
-  /// @param  req          - The request message to use for forwarding.
-  virtual int forward_request(pjsip_msg*& req) = 0;
+  /// This function may be called while processing initial requests,
+  /// in-dialog requests and cancels but not during response handling
+  ///
+  /// @param  req          - The request message to use for forwarding.  If NULL
+  ///                        the original request message is used.
+  virtual void forward_request(pjsip_msg* req=NULL) = 0;
 
   /// Indicate that the response should be forwarded following standard routing
   /// rules.  Note that, if this service created multiple forks, the responses
   /// will be aggregated before being sent downstream.
-  /// 
+  ///
   /// This function may be called while handling any response.
   ///
   /// @param  rsp          - The response message to use for forwarding.
   virtual void forward_response(pjsip_msg*& rsp) = 0;
 
-  /// Rejects the request with the specified status code and text.
-  /// 
+#if 0
+// AMC The following API call is needed to allow ASs that call out to other
+// SIP devices to process a call, or to allow asynchronous handling of
+// requests (e.g. database dips).  Lifetimes of objects are hard to manage -
+// possibly this function should return a special callback object to use?
+
+  /// Defer handling a request until a later time.  The service code should
+  /// keep a reference to the Once the asynchronous
+  /// operation has been completed, the service should hold on to the
+  /// ServiceTxsHelper and call into one of the other transaction actions to
+  /// indicate the desired behaviour.  Services must be prepared for the
+  /// transaction to be cancelled at any time.
+  ///
+  /// This function may be called during any of the entry point functions.
+  ///
+  virtual void defer_request() = 0;
+
+// AMC the following function is needed to build a B2BUA or an AS that creates
+// OOTB calls.  Somehow need to add a way for a service to get hold of a
+// ServiceTxsHelper when not in a transaction.
+
+  /// Create and send a new request.  This function may be called at any time.
+  ///
+  /// The service is automatically added to the newly created dialog.
+  ///
+  /// @param  req         - The request message to use.
+  virtual void create_request(pjsip_msg* req);
+#endif
+
+  /// Reject the original request with the specified status code and text.
+  ///
   /// This method can only be called when handling any non-cancel request.
   ///
   /// @param  status_code  - The SIP status code to send on the response.
@@ -152,71 +178,19 @@ public:
   /// related to this service invocation.
   virtual SAS::TrailId trail() const = 0;
 
-protected:
-  /// Constructor
-  AppServerTsxHelper(ServiceTsxHelper* helper) : _helper(helper) {};
-
-private:
-  /// Transaction helper to delegate to for underlying service-related processing.
-  /// This has a more powerful and flexible API than the one offered by
-  /// AppServerTsxHelper and should not be directly exposed on the public API.
-  ServiceTsxHelper* _helper;
 };
 
 
-/// The AppServer class is an abstract base class used to implement services.
-///
-/// Derived classes are instantiated during system initialization and 
-/// register a service name with Sprout.  Sprout calls the get_app_tsx method
-/// on an AppServer derived class when
-///
-/// -  an IFC triggers with ServiceName containing a host name of the form
-///    &lt;service_name&gt;.&lt;homedomain&gt;
-/// -  a request is received for a dialog where the service previously called
-///    add_to_dialog.
-///
-class AppServer
-{
-public:
-  /// Virtual destructor.
-  virtual ~AppServer() {}
-
-  /// Called when the system determines the service should be invoked for a
-  /// received request.  The AppServer can either return NULL indicating it
-  /// does not want to process the request, or create a suitable object
-  /// derived from the AppServerTsx class to process the request.
-  ///
-  /// @param  helper        - The service helper to use to perform
-  ///                         the underlying service-related processing.
-  /// @param  req           - The received request message.
-  virtual AppServerTsx* get_app_tsx(AppServerTsxHelper* helper,
-                                    pjsip_msg* req) = 0;
-
-  /// Returns the name of this service.
-  const std::string service_name() { return _service_name; }
-
-protected:
-  /// Constructor.
-  AppServer(const std::string& service_name) :
-    _service_name(service_name) {}
-
-private:
-  /// The name of this service.
-  const std::string _service_name;
-
-};
-
-
-/// The AppServerTsx class is an abstract base class used to handle the
+/// The ServiceTsx class is an abstract base class used to handle the
 /// application-server-specific processing of a single transaction.  It
-/// encapsulates an AppServerTsxHelper, which it calls through to to perform
-/// the underlying service-related processing.
+/// is provided with a ServiceTsxHelper, which it may use to perform the
+/// underlying service-related processing.
 ///
-class AppServerTsx
+class ServiceTsx
 {
 public:
   /// Virtual destructor.
-  virtual ~AppServerTsx() {}
+  virtual ~ServiceTsx() {}
 
   /// Called for an initial request (dialog-initiating or out-of-dialog) with
   /// the original received request for the transaction.
@@ -277,63 +251,61 @@ public:
 
 protected:
   /// Constructor.
-  AppServerTsx(AppServerTsxHelpersx* helper) : _helper(helper) {}
+  ServiceTsx(ServiceTsxHelper* helper) : _helper(helper) {}
 
-  /// Adds the service to the underlying SIP dialog with the specified dialog
-  /// identifier.
-  ///
-  /// @param  dialog_id    - The dialog identifier to be used for this service.
-  ///                        If omitted, a default unique identifier is created
-  ///                        using parameters from the SIP request.
-  ///
-  void add_to_dialog(const std::string& dialog_id="")
-    {_helper->add_to_dialog(dialog_id);}
-
-  /// Returns the dialog identifier for this service.
-  ///
-  /// @returns             - The dialog identifier attached to this service,
-  ///                        either by this ServiceTsx instance
-  ///                        or by an earlier transaction in the same dialog.
-  const std::string& dialog_id() const
-    {return _helper->dialog_id();}
-
-  /// Clones the request.  This is typically used when forking a request if
-  /// different request modifications are required on each fork.
-  ///
-  /// @returns             - The cloned request message.
-  /// @param  req          - The request message to clone.
-  pjsip_msg* clone_request(pjsip_msg* req)
-    {return _helper->clone_request(req);}
-
-// AMC - Wrapper functions around the message control functions, similar to
-// the surrounding functions.
-
-  /// Frees the specified message.  Received responses or messages that have
-  /// been cloned with add_target are owned by the AppServerTsx.  It must
-  /// call into ServiceTsx either to send them on or to free them (via this
-  /// API).
-  ///
-  /// @param  msg          - The message to free.
-  void free_msg(pjsip_msg* msg)
-    {return _helper->free_msg(msg);}
-
-  /// Returns the pool corresponding to a message.  This pool can then be used
-  /// to allocate further headers or bodies to add to the message.
-  ///
-  /// @returns             - The pool corresponding to this message.
-  /// @param  msg          - The message.
-  pj_pool_t* get_pool(const pjsip_msg* msg)
-    {return _helper->get_pool(msg);}
-
-  /// Returns the SAS trail identifier that should be used for any SAS events
-  /// related to this service invocation.
-  SAS::TrailId trail() const
-    {return _helper->trail();}
+  /// AMC - Add wrapper functions here to call through to the
+  /// helper (as for AppServerTsx).
 
 private:
-  /// Transaction context to use for underlying service-related processing.
-  ServiceTsx* _helper;
+  /// Transaction helper to use for underlying service-related processing.
+  ServiceTsxHelper* _helper;
+}
 
+
+/// The AbstractService class is a base class on which SIP services can be
+/// built.  
+///
+/// Derived classes are instantiated during system initialization and 
+/// register a service name with Sprout.  Sprout calls the create_tsx method
+/// on an AbstractService derived class when the ServiceManager determines that
+/// the next hop for a request contains a hostname of the form
+/// &lt;service_name&gt;.&lt;homedomain&gt;.  This may happen if:
+///
+/// -  an initial request is received with a top route header/ReqURI indicating
+///    this service.
+/// -  an initial request has been forwarded by some earlier service instance
+///    to this service.
+/// -  an in-dialog request is received for a dialog on which the service
+///    previously called add_to_dialog.
+///
+class AbstractService
+{
+public:
+  /// Virtual destructor.
+  virtual ~AbstractService() {}
+
+  /// Called when the system determines the service should be invoked for a
+  /// received request.  The AbstractService can either return NULL indicating it
+  /// does not want to process the request, or create a suitable object
+  /// derived from the ServiceTsx class to process the request.
+  ///
+  /// @param  helper        - The service helper to use to perform
+  ///                         the underlying service-related processing.
+  /// @param  req           - The received request message.
+  virtual ServiceTsx* get_app_tsx(ServiceTsxHelper* helper,
+                                  pjsip_msg* req) = 0;
+
+  /// Returns the name of this service.
+  const std::string service_name() { return _service_name; }
+
+protected:
+  /// Constructor.
+  AbstractService(const std::string& service_name) :
+    _service_name(service_name) {}
+
+private:
+  /// The name of this service.
+  const std::string _service_name;
 };
 
 #endif
