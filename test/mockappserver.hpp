7722c5a4c446b1e5fd1f909e5f6292a29ba2a30e
Mike Evans <mike@metaswitch.com>
2014-07-30 14:46:14 +0000

    Fix up mock appserver for new APIs

--- test/mockappserver.hpp
+++ test/mockappserver.hpp
@@ -64,8 +64,12 @@ public:
   MOCK_METHOD3(create_response, pjsip_msg*(pjsip_msg*, pjsip_status_code, const std::string&));
   MOCK_METHOD1(send_request, int(pjsip_msg*&));
   MOCK_METHOD1(send_response, void(pjsip_msg*&));
+  MOCK_METHOD1(cancel_fork, void(int));
   MOCK_METHOD1(free_msg, void(pjsip_msg*&));
   MOCK_METHOD1(get_pool, pj_pool_t*(const pjsip_msg*));
+  MOCK_METHOD3(schedule_timer, bool(int, void*, int));
+  MOCK_METHOD1(cancel_timer, void(int));
+  MOCK_METHOD1(timer_running, bool(int));
 };
 
 
