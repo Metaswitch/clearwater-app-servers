58a8216c2a39bcadaf0636e002abed67539b0118
Mike Evans <mike@metaswitch.com>
2014-08-18 14:58:07 +0000

    Add route_hdr() API

--- include/appserver.h
+++ include/appserver.h
@@ -84,6 +84,16 @@ public:
   ///
   virtual pjsip_msg* original_request() = 0;
 
+  /// Returns the top Route header from the original incoming request.  This
+  /// can be inpsected by the app server, but should not be modified.  Note that
+  /// this Route header is removed from the request passed to the app server on
+  /// the on_*_request calls.
+  ///
+  /// @returns             - A pointer to a read-only copy of the top Route
+  ///                        header from the received request.
+  ///
+  virtual const pjsip_route_hdr* route_hdr() const = 0;
+
   /// Adds the service to the underlying SIP dialog with the specified dialog
   /// identifier.
   ///
@@ -325,6 +335,17 @@ protected:
   ///
   pjsip_msg* original_request()
     {return _helper->original_request();}
+
+  /// Returns the top Route header from the original incoming request.  This
+  /// can be inpsected by the app server, but should not be modified.  Note that
+  /// this Route header is removed from the request passed to the app server on
+  /// the on_*_request calls.
+  ///
+  /// @returns             - A pointer to a read-only copy of the top Route
+  ///                        header from the received request.
+  ///
+  const pjsip_route_hdr* route_hdr() const
+    {return _helper->route_hdr();}
 
   /// Adds the service to the underlying SIP dialog with the specified dialog
   /// identifier.
