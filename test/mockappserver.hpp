26646df6436010b59cdf831a1f709c9a4e63f485
Colin Haywood <colin.haywood@metaswitch.com>
2017-11-21 12:00:25 +1300

    Add diagnostics when Sprout sends a CANCEL
    Changes to appserver.h

--- test/mockappserver.hpp
+++ test/mockappserver.hpp
@@ -37,7 +37,7 @@ public:
   MOCK_METHOD3(create_response, pjsip_msg*(pjsip_msg*, pjsip_status_code, const std::string&));
   MOCK_METHOD1(send_request, int(pjsip_msg*&));
   MOCK_METHOD1(send_response, void(pjsip_msg*&));
-  MOCK_METHOD2(cancel_fork, void(int, int));
+  MOCK_METHOD3(cancel_fork, void(int, int, std::string));
   MOCK_METHOD1(free_msg, void(pjsip_msg*&));
   MOCK_METHOD1(get_pool, pj_pool_t*(const pjsip_msg*));
   MOCK_METHOD3(schedule_timer, bool(void*, TimerID&, int));
